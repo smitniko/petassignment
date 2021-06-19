c8af7858be2805357496038d92571b59b0e25105
Nikolas Smith <smitniko@sheridancollege.ca>
2021-06-19 15:47:50 -0400

    Updated codebase

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO pet
+(first_name, last_name, pet_type, pet_gender, pet_vaccination)
+VALUES
+('Mr','Fluffy','Dog',2,true),
+('Trevor','SassyPants','Cat',1,false),
+('Big','Ears','Rabbit',2,true),
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pet (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         first_name VARCHAR(30),
+                         last_name VARCHAR(30),
+                         pet_type VARCHAR(30),
+                         pet_gender INTEGER,
+                         pet_vaccination BOOLEAN
+);
